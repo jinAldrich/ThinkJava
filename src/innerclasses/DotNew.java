f6c70f772d12d70096c068d5cb83bdf168b31aff
yujin <jin.yu@brighter-wireless.com>
2014-11-04 17:45:04 +0800

    study '.this' and '.new' key-word in DotThis.java 2014-11-4

--- src/innerclasses/DotNew.java
+++ src/innerclasses/DotNew.java
@@ -7,5 +7,6 @@ public class DotNew {
   public static void main(String[] args) {
     DotNew dn = new DotNew();
     DotNew.Inner dni = dn.new Inner();
+    DotNew.Inner dnin = new DotNew().new Inner();
   }
 } ///:~
--- src/innerclasses/DotThis.java
+++ src/innerclasses/DotThis.java
@@ -1,21 +1,31 @@
 package innerclasses;
+
 //: innerclasses/DotThis.java
 // Qualifying access to the outer-class object.
 
 public class DotThis {
-  void f() { System.out.println("DotThis.f()"); }
-  public class Inner {
-    public DotThis outer() {
-      return DotThis.this;
-      // A plain "this" would be Inner's "this"
-    }
-  }
-  public Inner inner() { return new Inner(); }
-  public static void main(String[] args) {
-    DotThis dt = new DotThis();
-    DotThis.Inner dti = dt.inner();
-    dti.outer().f();
-  }
-} /* Output:
-DotThis.f()
-*///:~
+	void f() {
+		System.out.println("DotThis.f()");
+	}
+
+	public class Inner {
+		public DotThis outer() {
+			System.out.println(this); //Inner's "this"
+			System.out.println(DotThis.this); //outer class DotThis's "this"
+			return DotThis.this;
+			// A plain "this" would be Inner's "this"
+		}
+	}
+
+	public Inner inner() {
+		return new Inner();
+	}
+
+	public static void main(String[] args) {
+		DotThis dt = new DotThis();
+		DotThis.Inner dti = dt.inner();
+		dti.outer().f();
+	}
+} /*
+ * Output: DotThis.f()
+ */// :~
